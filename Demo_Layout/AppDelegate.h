37f6b0ff3e063cd9fd46778c185b29e88cad5813
liudukun <liudukun@126.com>
2017-02-14 16:03:58 +0800

    Initial Commit

---
+++ Demo_Layout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo_Layout
+//
+//  Created by liudukun on 2017/2/14.
+//  Copyright © 2017年 liudukun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo_Layout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo_Layout
+//
+//  Created by liudukun on 2017/2/14.
+//  Copyright © 2017年 liudukun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
